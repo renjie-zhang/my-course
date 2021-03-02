1b890636b4472f80dcb0abb5a205563e896ab043
renjie zhang <joker_zhang@aliyun.com>
2021-03-02 23:47:24 +0800

    finish one

--- java_training_camp/first-homework/my-web-mvc/src/main/java/com/renjiezhang/web/mvc/handler/HeaderWriter.java
+++
@@ -1,9 +0,0 @@
-package com.renjiezhang.web.mvc.handler;
-
-import java.util.List;
-import java.util.Map;
-
-public interface HeaderWriter {
-
-    void write(Map<String, List<String>> headers,String... headerValues);
-}
